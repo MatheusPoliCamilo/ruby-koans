2b725fb1162f0f701aa385238af9e3229358a726
MatheusPoli <matheuspolicamilo@gmail.com>
2019-07-23 13:06:47 -0300

    Path 160

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  triangle = [a, b, c]
+
+  if triangle.uniq.length == 3
+    return :scalene
+  end
+
+  if triangle.uniq.length == 2
+    return :isosceles
+  end
+
+  :equilateral
 end
 
 # Error class used in part 2.  No need to change this code.
